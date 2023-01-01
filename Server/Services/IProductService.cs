af1cf3a089390862089b9bec3c9df86becffaa58
Максим Зимак <mzimak18@gmail.com>
2023-01-01 22:22:06 +0100

    Finish of ProductDetails page

--- Server/Services/IProductService.cs
+++ Server/Services/IProductService.cs
@@ -5,6 +5,6 @@ namespace Ecomm.Server.Services
     public interface IProductService
     {
         Task<ServiceResponse<List<Products>>> GetProductsAsync();
-
+        Task<ServiceResponse<Products>> GetProductAsync(int productId);
     }
 }
--- Server/Services/ProductService.cs
+++ Server/Services/ProductService.cs
@@ -8,6 +8,23 @@
             _context = context;
         }
 
+        public async Task<ServiceResponse<Products>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<Products>();
+            var product = await _context.ProductsC.FindAsync(productId);
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Product does not exist.";
+            }
+            else
+            {
+                response.Data = product;
+            }
+
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Products>>> GetProductsAsync()
         {
             var response = new ServiceResponse<List<Products>>
